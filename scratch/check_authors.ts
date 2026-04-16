86d1ad29e4570be8f14d5d6773006dabe04c864d
Fazil <zorxdxb@gmail.com>
2026-04-16 15:36:39 +0400

    Fix build blocker: remove temporary scratch scripts

--- scratch/check_authors.ts
+++
@@ -1,8 +0,0 @@
-import { getAuthors } from "./lib/authors.ts";
-
-async function main() {
-    const authors = await getAuthors();
-    console.log(JSON.stringify(authors, null, 2));
-}
-
-main();
--- scratch/check_blog_authors.ts
+++
@@ -1,9 +0,0 @@
-import { getBlogs } from "./lib/blogs.ts";
-
-async function main() {
-    const blogs = await getBlogs();
-    const authorsInBlogs = [...new Set(blogs.map(b => b.author))];
-    console.log("Authors in blogs table column 'author':", authorsInBlogs);
-}
-
-main();
