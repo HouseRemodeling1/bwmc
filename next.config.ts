5ae15cbd33f33eb9e9298023b6716653afee33d9
MCQsDrive Blog <raoali5476@gmail.com>
2026-07-29 01:34:32 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
@@ -42,6 +41,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/investors',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
 };
-
 export default nextConfig;
